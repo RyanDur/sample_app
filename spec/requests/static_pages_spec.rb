bce238b740e23e1b62fd6ef0264a39987548cd81
RyanDurling <ryan.p.durling@gmail.com>
2012-11-19 12:03:02 -0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,37 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  describe "Static Pages" do
-    subject {page}
+  subject {page}
 
-    describe "Home Page" do
-      before {visit root_path}
+  describe "Home Page" do
+    before {visit root_path}
 
-      it {should have_selector 'h1', text: 'Sample App'}
-      it {should have_selector 'title', text: full_title('')}
-      it {should_not have_selector 'title', text: '| Home'}
-    end
+    it {should have_selector 'h1', text: 'Sample App'}
+    it {should have_selector 'title', text: full_title('')}
+    it {should_not have_selector 'title', text: '| Home'}
+  end
 
-    describe "Help Page" do
-      before {visit help_path}
+  describe "Help Page" do
+    before {visit help_path}
 
-      it {should have_selector 'h1', text: 'Help'}
-      it {should have_selector 'title', text: full_title('Help')}
-    end
+    it {should have_selector 'h1', text: 'Help'}
+    it {should have_selector 'title', text: full_title('Help')}
+  end
 
-    describe "About page" do
-      before {visit about_path}
+  describe "About page" do
+    before {visit about_path}
 
-      it {should have_selector 'h1', text: 'About Us'}
-      it {should have_selector 'title', text: full_title('About Us')}
-    end
+    it {should have_selector 'h1', text: 'About Us'}
+    it {should have_selector 'title', text: full_title('About Us')}
+  end
 
-    describe "Contact page" do
-      before {visit contact_path}
+  describe "Contact page" do
+    before {visit contact_path}
 
-      it {should have_selector 'h1', text: "Contact"}
-      it {should have_selector 'title', text: full_title('Contact')}
-    end
-
+    it {should have_selector 'h1', text: "Contact"}
+    it {should have_selector 'title', text: full_title('Contact')}
   end
 end
