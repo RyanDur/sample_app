e714c16eae305e3e816994bf01f1857b0d6578c0
RyanDurling <ryan.p.durling@gmail.com>
2013-01-18 08:55:01 -0800

    Add Contact to static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,29 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  describe "Home Page" do
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector 'h1', :text => 'Sample App'
-    end
+  let(:base_title) {"Ruby on Rails Tutorial Sample App | "}
 
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector 'title',
-      :text => "Ruby on Rails Tutorial Sample App | Home"
-    end
+  describe "Home Page" do
+    before {visit '/static_pages/home'}
+    it {page.should have_selector 'h1', :text => 'Sample App'}
+    it {page.should have_selector 'title', :text => "#{base_title}Home"}
   end
 
   describe "Help Page" do
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector 'h1', :text => 'Help'
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector 'title',
-      :text => "Ruby on Rails Tutorial Sample App | Help"
-    end
+    before {visit '/static_pages/help'}
+    it {page.should have_selector 'h1', :text => 'Help'}
+    it {page.should have_selector 'title', :text => "#{base_title}Help"}
   end
 
   describe "About Page" do
-    it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector 'h1', :text => 'About Us'
-    end
+    before {visit '/static_pages/about'}
+    it {page.should have_selector 'h1', :text => 'About Us'}
+    it {page.should have_selector 'title', :text => "#{base_title}About Us"}
+  end
 
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector 'title',
-      :text => "Ruby on Rails Tutorial Sample App | About Us"
-    end
+  describe "Contact Page" do
+    before {visit '/static_pages/contact'}
+    it {page.should have_selector 'h1', :text => 'Contact'}
+    it {page.should have_selector 'title', :text => "#{base_title}Contact"}
   end
 end
