3c3097e23d96e54f766de5564fd878d77fcccf52
RyanDurling <ryan.p.durling@gmail.com>
2013-01-19 03:45:17 -0800

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,29 +2,30 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) {"Ruby on Rails Tutorial Sample App | "}
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
 
   describe "Home Page" do
     before {visit '/static_pages/home'}
     it {page.should have_selector 'h1', :text => 'Sample App'}
-    it {page.should have_selector 'title', :text => "#{base_title}Home"}
+    it {page.should have_selector 'title', :text => "#{base_title}"}
+    it {page.should_not have_selector 'title', :text => "Home"}
   end
 
   describe "Help Page" do
     before {visit '/static_pages/help'}
     it {page.should have_selector 'h1', :text => 'Help'}
-    it {page.should have_selector 'title', :text => "#{base_title}Help"}
+    it {page.should have_selector 'title', :text => "#{base_title} | Help"}
   end
 
   describe "About Page" do
     before {visit '/static_pages/about'}
     it {page.should have_selector 'h1', :text => 'About Us'}
-    it {page.should have_selector 'title', :text => "#{base_title}About Us"}
+    it {page.should have_selector 'title', :text => "#{base_title} | About Us"}
   end
 
   describe "Contact Page" do
     before {visit '/static_pages/contact'}
     it {page.should have_selector 'h1', :text => 'Contact'}
-    it {page.should have_selector 'title', :text => "#{base_title}Contact"}
+    it {page.should have_selector 'title', :text => "#{base_title} | Contact"}
   end
 end
